0662c32253f9024784c5f5224e687bc19b44f68a
leafypixiestix <justincai.lps@gmail.com>
2016-04-21 21:13:00 -0600

    Updated training set with more REL tagged tweets

--- label_more_training.py
+++ label_more_training.py
@@ -53,14 +53,31 @@ print("created training set")
 classifier = nltk.classify.SklearnClassifier(LogisticRegression())
 classifier.train(train_set) 
 
+correct = 0
+incorrect = 0
 for file in listdir(test_dir):
     print(r"{}\{}".format(test_dir, file))
     rel_count = 0
     irr_count = 0
     for line in open(r"{}\{}".format(test_dir, file), "r", encoding="utf8"):
         parsed = json.loads(line)
-        if classifier.classify(tf.featurize(parsed["text"])) == "REL":
-            rel_count = rel_count + 1
+        tweet = (parsed["text"], classifier.classify(tf.featurize(parsed["text"])))
+        if  tweet[1] == "REL":
+            if tweet not in tweets:     
+                rel_count = rel_count + 1
+                print("{}\n1:REL 2:IRR".format(parsed["text"]))
+                n = ''
+                while n not in ("1","2"):
+                    n = input()
+                if n == "1":
+                    correct = correct + 1
+                    tweets.append(tweet)
+                else:
+                    incorrect = incorrect + 1
         else:
             irr_count = irr_count + 1
     print("REL: {} IRR: {}".format(rel_count, irr_count))
+
+file = open("{}.new".format(train_file), "w", encoding="utf8")
+for (text, label) in tweets:
+    file.write("{}\n".format(repr({"text":text, "type": label})))
