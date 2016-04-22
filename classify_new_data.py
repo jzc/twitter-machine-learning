64bcc1a610da61cf4ae66eb7a3f9c9c1389417a6
leafypixiestix <justincai.lps@gmail.com>
2016-04-22 14:38:19 -0600

    Added code to classify new data

---
+++ classify_new_data.py
@@ -0,0 +1,39 @@
+from twitter_machine_learning import load_tweets, TweetFeaturizer, create_train_set, create_logistic_classifier
+from os import listdir
+import json
+
+train_file = r".\train_set_1.json"
+new_dir = r".\flu_related_tweets"
+classified_dir = r".\classified_tweets"
+
+print("Loading tweets")
+tweets = load_tweets(train_file)
+print("Done")
+
+print("Creating featurizer")
+tf = TweetFeaturizer(tweets, 4, 3)
+print("Done")
+
+print("Creating training set")
+train_set = create_train_set(tf, tweets)
+print("Done")
+
+print("Creating classifier")
+classifier = create_logistic_classifier(train_set)
+print("Done")
+
+for file in listdir(new_dir):
+    rel_count = 0
+    irr_count = 0
+    out = open(r"{}\{}".format(classified_dir,file), "w", encoding="utf8")
+    print(r"{}\{}".format(new_dir, file))
+    for line in open(r"{}\{}".format(new_dir, file), "r", encoding="utf8"):
+        parsed = json.loads(line)
+        label = classifier.classify(tf.featurize(parsed["text"]))
+        if label == "REL":
+            rel_count = rel_count + 1
+        else:
+            irr_count = irr_count + 1
+        out.write(json.JSONEncoder().encode({"text":parsed["text"], "created_at":parsed["created_at"], "type":label}) + "\n")
+    print("REL: {} IRR: {}".format(rel_count, irr_count))
+        
--- visualize_heat.py
+++ visualize_heat.py
@@ -2,38 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+import json
 
 NaiveP = [[],[],[]]
 LogP = [[],[],[]]
 SvcP = [[],[],[]]
 
-file = open(".\classifier_results.txt", "r")
-lines = file.readlines()
-for i in range(0,len(lines),10):
-    current = lines[i:i+10]
-    average = 0
-    for data in current:
-        data = data.split(", ")   
-        average = average + float(data[1]) #1: accuracy 2:precision 3:recall     
-    average = average/10
-    name = data[0].split()
-    x,y,name = int(name[2]), int(name[4]), name[0]
-    if name == "NaiveBayes":
-        NaiveP[0].append(x)
-        NaiveP[1].append(y)
-        NaiveP[2].append(average)
-    elif name == "LogisticRegression":
-        LogP[0].append(x)
-        LogP[1].append(y)
-        LogP[2].append(average)
-    elif name == "SVC":
-        SvcP[0].append(x)
-        SvcP[1].append(y)
-        SvcP[2].append(average)
-        
+file = open(r".\logistic_results.json", "r")
+results = []
+for line in file:
+    parsed = json.loads(line)
+    results.append(parsed)
+
 xlabels = np.arange(1,6)
 ylabels = np.arange(1,11)
-z = np.array(LogP[2])
+z = np.array([result["average"]["recall"] for result in results])
 z.resize(len(ylabels), len(xlabels))
 fig, ax = plt.subplots()
 
